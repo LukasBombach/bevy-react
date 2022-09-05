dbb0249d05bd5ec7e2565ba8f421e4ca29312577
Lukas Bombach <lukas.bombach@googlemail.com>
2022-09-05 09:54:35 +0200

    reformatted stuff

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,6 @@ struct Name(String);
 
 struct GreetTimer(Timer);
 
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-    // update our timer with the time elapsed since the last update
-    // if that caused the timer to finish, we say hello to everyone
-    if timer.0.tick(time.delta()).just_finished() {
-        for name in query.iter() {
-            println!("hello {}!", name.0);
-        }
-    }
-}
-
 pub struct HelloPlugin;
 
 impl Plugin for HelloPlugin {
@@ -25,6 +15,16 @@ impl Plugin for HelloPlugin {
         app.insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
             .add_startup_system(add_people)
             .add_system(greet_people);
+    }
+}
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    // update our timer with the time elapsed since the last update
+    // if that caused the timer to finish, we say hello to everyone
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in query.iter() {
+            println!("hello {}!", name.0);
+        }
     }
 }
 
