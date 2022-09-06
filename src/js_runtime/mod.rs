6ade018342e56b50a6ffafd88a8a8989b3719efc
Lukas Bombach <lukas.bombach@googlemail.com>
2022-09-06 13:44:18 +0200

    integrated deno

--- src/js_runtime/mod.rs
+++ src/js_runtime/mod.rs
@@ -1,7 +1,9 @@
 use crate::LoadedFont;
 use bevy::prelude::*;
 use crossbeam_channel::{bounded, Receiver};
+use deno_core::error::AnyError;
 use rand::Rng;
+use std::rc::Rc;
 use std::time::{Duration, Instant};
 
 #[derive(Deref)]
@@ -24,17 +26,22 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn_bundle(Camera2dBundle::default());
 
     let (tx, rx) = bounded::<u32>(10);
-    std::thread::spawn(move || loop {
-        // Everything here happens in another thread
-        // This is where you could connect to an external data source
-        let mut rng = rand::thread_rng();
-        let start_time = Instant::now();
-        let duration = Duration::from_secs_f32(rng.gen_range(0.0..0.2));
-        while start_time.elapsed() < duration {
-            // Spinning for 'duration', simulating doing hard work!
-        }
+    std::thread::spawn(move || {
+        // let mut rng = rand::thread_rng();
+        // let start_time = Instant::now();
+        // let duration = Duration::from_secs_f32(rng.gen_range(0.0..0.2));
+        // while start_time.elapsed() < duration {
+        // }
+        // tx.send(rng.gen_range(0..2000)).unwrap();
 
-        tx.send(rng.gen_range(0..2000)).unwrap();
+        let runtime = tokio::runtime::Builder::new_current_thread()
+            .enable_all()
+            .build()
+            .unwrap();
+
+        if let Err(error) = runtime.block_on(run_js("./src/main.js")) {
+            eprintln!("error: {}", error);
+        }
     });
 
     commands.insert_resource(StreamReceiver(rx));
@@ -46,4 +53,17 @@ fn read_stream(receiver: ResMut<StreamReceiver>, mut events: EventWriter<StreamE
     for from_stream in receiver.try_iter() {
         events.send(StreamEvent(from_stream));
     }
+}
+
+async fn run_js(file_path: &str) -> Result<(), AnyError> {
+    let main_module = deno_core::resolve_path(file_path)?;
+    let mut js_runtime = deno_core::JsRuntime::new(deno_core::RuntimeOptions {
+        module_loader: Some(Rc::new(deno_core::FsModuleLoader)),
+        ..Default::default()
+    });
+
+    let mod_id = js_runtime.load_main_module(&main_module, None).await?;
+    let result = js_runtime.mod_evaluate(mod_id);
+    js_runtime.run_event_loop(false).await?;
+    result.await?
 }
