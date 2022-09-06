6bb35f8880c59891f6967a531b55f83bc3713d86
Lukas Bombach <lukas.bombach@googlemail.com>
2022-09-06 13:06:35 +0200

    thread example

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,89 @@
 use bevy::prelude::*;
-
-#[derive(Component)]
-struct Person;
-
-#[derive(Component)]
-struct Name(String);
-
-struct GreetTimer(Timer);
-
-pub struct HelloPlugin;
-
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
-            .add_startup_system(add_people)
-            .add_system(greet_people);
-    }
-}
-
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-    // update our timer with the time elapsed since the last update
-    // if that caused the timer to finish, we say hello to everyone
-    if timer.0.tick(time.delta()).just_finished() {
-        for name in query.iter() {
-            println!("hello {}!", name.0);
-        }
-    }
-}
-
-fn add_people(mut commands: Commands) {
-    commands
-        .spawn()
-        .insert(Person)
-        .insert(Name("Elaina Proctor".to_string()));
-    commands
-        .spawn()
-        .insert(Person)
-        .insert(Name("Renzo Hume".to_string()));
-    commands
-        .spawn()
-        .insert(Person)
-        .insert(Name("Zayna Nieves".to_string()));
-}
+// Using crossbeam_channel instead of std as std `Receiver` is `!Sync`
+use crossbeam_channel::{bounded, Receiver};
+use rand::Rng;
+use std::time::{Duration, Instant};
 
 fn main() {
     App::new()
+        .add_event::<StreamEvent>()
         .add_plugins(DefaultPlugins)
-        .add_plugin(HelloPlugin)
+        .add_startup_system(setup)
+        .add_system(read_stream)
+        .add_system(spawn_text)
+        .add_system(move_text)
         .run();
+}
+
+#[derive(Deref)]
+struct StreamReceiver(Receiver<u32>);
+struct StreamEvent(u32);
+
+#[derive(Deref)]
+struct LoadedFont(Handle<Font>);
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn_bundle(Camera2dBundle::default());
+
+    let (tx, rx) = bounded::<u32>(10);
+    std::thread::spawn(move || loop {
+        // Everything here happens in another thread
+        // This is where you could connect to an external data source
+        let mut rng = rand::thread_rng();
+        let start_time = Instant::now();
+        let duration = Duration::from_secs_f32(rng.gen_range(0.0..0.2));
+        while start_time.elapsed() < duration {
+            // Spinning for 'duration', simulating doing hard work!
+        }
+
+        tx.send(rng.gen_range(0..2000)).unwrap();
+    });
+
+    commands.insert_resource(StreamReceiver(rx));
+    commands.insert_resource(LoadedFont(asset_server.load("fonts/FiraSans-Bold.ttf")));
+}
+
+// This system reads from the receiver and sends events to Bevy
+fn read_stream(receiver: ResMut<StreamReceiver>, mut events: EventWriter<StreamEvent>) {
+    for from_stream in receiver.try_iter() {
+        events.send(StreamEvent(from_stream));
+    }
+}
+
+fn spawn_text(
+    mut commands: Commands,
+    mut reader: EventReader<StreamEvent>,
+    loaded_font: Res<LoadedFont>,
+) {
+    let text_style = TextStyle {
+        font: loaded_font.clone(),
+        font_size: 20.0,
+        color: Color::WHITE,
+    };
+
+    for (per_frame, event) in reader.iter().enumerate() {
+        commands.spawn_bundle(Text2dBundle {
+            text: Text::from_section(event.0.to_string(), text_style.clone())
+                .with_alignment(TextAlignment::CENTER),
+            transform: Transform::from_xyz(
+                per_frame as f32 * 100.0 + rand::thread_rng().gen_range(-40.0..40.0),
+                300.0,
+                0.0,
+            ),
+            ..default()
+        });
+    }
+}
+
+fn move_text(
+    mut commands: Commands,
+    mut texts: Query<(Entity, &mut Transform), With<Text>>,
+    time: Res<Time>,
+) {
+    for (entity, mut position) in &mut texts {
+        position.translation -= Vec3::new(0.0, 100.0 * time.delta_seconds(), 0.0);
+        if position.translation.y < -300.0 {
+            commands.entity(entity).despawn();
+        }
+    }
 }
